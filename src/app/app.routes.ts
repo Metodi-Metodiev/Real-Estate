50d07815ab3ddb0920a7b95ef5821e7ffd6af8b6
Metodi <harisan_92@abv.bg>
2023-08-07 01:56:09 +0300

    login and header added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+      {
+        path: "",
+        loadComponent: () => import("./home/home.component").then(c => c.HomeComponent)
+      },
+
+      {
+        path: 'home',
+        loadComponent: () => import("./home/home.component").then(c => c.HomeComponent)
+      },
+      {
+        path: 'login',
+        loadComponent: () => import("./sig-in/sig-in.component").then(c => c.SigInComponent)
+      }
+];
---
+++ src/app/sig-in/sig-in.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sig-in',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sig-in.component.html',
+  styleUrls: ['./sig-in.component.css']
+})
+export class SigInComponent {
+
+}
